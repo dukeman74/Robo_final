a68b17a81061cf3a8deea7b7d0b55f07d4325298
Max <Maxsaldi0923@gmail.com>
2023-05-07 12:24:05 -0600

    stuff that dont work

--- controllers/grocery_shopper/grocery_shopper.py
+++ controllers/grocery_shopper/grocery_shopper.py
@@ -2,6 +2,7 @@
 
 # Nov 2, 2022
 
+import copy
 from controller import Robot, Keyboard
 import math
 import numpy as np
@@ -77,7 +78,7 @@ keyboard.enable(timestep)
 display = robot.getDevice("display")
 
 # Odometry
-pose_x     = 0
+pose_x     = -5
 pose_y     = 0
 pose_theta = 0
 
@@ -99,6 +100,10 @@ map = np.zeros(shape=[360,360])
 gripper_status="closed"
 state="menu"
 res = 300
+angle_threshold=math.pi/6
+distance_threshold=0.2
+d = None
+done = False
 
 def set_pose_via_truth():
     #get true pose data via gps compass'
@@ -235,8 +240,7 @@ def rrt(state_bounds, state_is_valid, starting_point, goal_point, k, delta_q):
             node_list.append(yaboi)
             if(directed and distance_nd(yaboi.point,goal_point)<10**-5):
                 return(yaboi) #only return winning path instead
-                return node_list
-
+            
     # TODO: Make sure to add every node you create onto node_list, and to set node.parent and node.path_from_parent for each
     print("big time miss")
     #return(Node(goal_point))
@@ -254,11 +258,11 @@ def RRT_plan(map,start,goal):
         if(map[pin[0]][pin[1]]==0):
             return(True)
         return(False)
-    path=rrt(bounds,valid,start,goal,1000,np.linalg.norm(bounds/10.))
+    path=rrt(bounds,valid,start,goal,10000,np.linalg.norm(bounds/10.))
+    plot_course(path)
     return(path)
 
-def plot_course(map,start,goal):
-    node_at=RRT_plan(map,start,goal)
+def plot_course(node_at):
     #for i in node_at:
     #    print("there was a node made at: ",i.point)
     #    map[i.point[0]][i.point[1]]=5
@@ -271,6 +275,106 @@ def plot_course(map,start,goal):
 
 #endregion
 
+#region drive
+def add_angles(wayp):
+    betterList = []
+    #print(wayP)
+    betterList.append([wayP[0][0],wayP[0][1],10])
+    for i in range(1,len(wayp)):
+        curr=wayp[i]
+        next=wayp[i-1]
+        t=math.atan2(next[1]-curr[1],next[0]-curr[0])
+        betterList.append([curr[0],curr[1],t])
+   #print("")
+    print(betterList)
+    return betterList
+        
+
+def drive_to_points(wayp):
+    global d
+    global done
+    global pose_x
+    global pose_y
+    global pose_theta
+
+    #for i in range(len(wayP)-1):
+        #print("wayP:", wayp[i])
+    way=wayp[-1]
+    xr=pose_x
+    xg=way[0]
+    yr=pose_y
+    yg=way[1]
+    tr=pose_theta
+    tg=way[2]
+    print("My xy: ", xr,yr , " My angle: " , tr , " Trying to make it to: ", xg,yg, " Angle I want: ", tg)
+    p=math.sqrt((xr-xg)**2+(yr-yg)**2)
+    if(d==None):
+        d=p
+    a=math.atan2(yg-yr,xg-xr)-tr
+    h=tg-tr
+    if(p<distance_threshold and not done):
+        #print("close enough")
+        if(abs(h)<angle_threshold):
+            #print("angle enough")
+            print("made waypoint ",len(wayp))
+            if len(wayp)==1:
+                done=True
+                vL=0
+                vR=0
+            else:
+                at=wayp.pop()
+                d=None
+    #more cursed code
+    if(d!= None and not done):
+        p1=1
+        p2=1
+        p3=0
+        rat = p/d
+
+        p2 = rat
+        p3 = (.2/(.2-1))*(rat-.2) *((1/p2))
+        """if(p<1):
+            p3=-.4
+        if(p<.5):
+            p3=.4
+        if(p<0.2):
+            p2=0.1"""
+        dx=p1*p
+        dtheta=p2*a+p3*h
+        #print("p3: ",p3,"p2:",p2,"dtheta:",dtheta, "p:",p, "im at:",xr,yr)
+        #dtheta*=-1
+
+    
+        # STEP 1: Inverse Kinematics Equations (vL and vR as a function dX and dTheta)
+        # Note that vL and vR in code is phi_l and phi_r on the slides/lecture
+        
+        
+        
+        # STEP 2.3: Proportional velocities
+        r=MAX_SPEED_MS/MAX_SPEED
+        vL = (dx-dtheta/2)/r # Left wheel velocity in rad/s
+        vR = (dx+dtheta/2)/r# Right wheel velocity in rad/s
+        #print("a: ",a,"leftvel:",vL,"rightvel:",vR,"im at:",xr,yr)
+        if abs(vL)>abs(vR):
+            vR*=MAX_SPEED/abs(vL)
+            vL=vL/abs(vL)*MAX_SPEED
+        else:
+            vL*=MAX_SPEED/abs(vR)
+            vR=vR/abs(vR)*MAX_SPEED
+        #print("a: ",a,"leftvel:",vL,"rightvel:",vR,"im at:",xr,yr)
+
+        # STEP 2.4: Clamp wheel speeds
+        if(vL>MAX_SPEED):
+            vL=MAX_SPEED
+        if(vL<-MAX_SPEED):
+            vL=-MAX_SPEED
+        if(vR>MAX_SPEED):
+            vR=MAX_SPEED
+        if(vR<-MAX_SPEED):
+            vR=-MAX_SPEED
+        return((vL,vR))
+    return((0,0))
+#endregion
 #region mapping functions
 
 def map_print():
@@ -348,6 +452,26 @@ def we_to_map(point):
     rx=round(rx)
     return((rx,ry))
 
+def map_to_we(point):
+    #webots top left = (16,10)
+    #webots bottom right = (-16,-10)
+    #map top left = (0,0)
+    #map bottom right = (360,360)
+    rx=point[0] 
+    ry=point[1]
+    t=rx
+    rx=ry
+    ry=t
+    ry-=67
+    scale=360./32. #to scale the y axis up to the map size
+    rx/=scale #(0,360)
+    ry/=scale #(0,225)
+    rx-=16 #(0,32)
+    ry-=10 #(0,20)
+    wx=-rx #(-16,16)
+    wy=-ry 
+    return((wx,wy))
+
 def colorize(g):
     if(g> 1 or g < 0):
        #print("threek")
@@ -378,7 +502,13 @@ while robot.step(timestep) != -1:
             plt.imshow(map_blocks.T)
             plt.show()
             set_pose_via_truth()
-            plot_course(map_blocks,we_to_map((pose_x,pose_y)),we_to_map((10,1.8)))
+            path = RRT_plan(map_blocks,we_to_map((pose_x,pose_y)),we_to_map((10,1.8)))
+            wayP = []
+            while path.parent != None:
+                wayP.append(map_to_we([path.point[0],path.point[1]]))
+                path = path.parent
+            wayP = add_angles(wayP)
+            state = 'drive'
             
         elif key == ord('M'):   #enter mapping mode
             state="map"
@@ -393,11 +523,12 @@ while robot.step(timestep) != -1:
             np.save("map.npy",(map>.55)*1)
             display.imageSave(None,"map.png") 
             print("Map file saved")
-
-        
-    
-    robot_parts["wheel_left_joint"].setVelocity(vL)
-    robot_parts["wheel_right_joint"].setVelocity(vR)
+    if(state=="drive"):
+        set_pose_via_truth()
+        v = drive_to_points(wayP)
+        print(v)
+        robot_parts["wheel_left_joint"].setVelocity(v[0])
+        robot_parts["wheel_right_joint"].setVelocity(v[1])
     
     if(gripper_status=="open"):
         # Close gripper, note that this takes multiple time steps...
@@ -407,6 +538,8 @@ while robot.step(timestep) != -1:
             gripper_status="closed"
     else:
         # Open gripper
+        robot_parts["wheel_left_joint"].setVelocity(0)
+        robot_parts["wheel_right_joint"].setVelocity(0)
         robot_parts["gripper_left_finger_joint"].setPosition(0.045)
         robot_parts["gripper_right_finger_joint"].setPosition(0.045)
         if left_gripper_enc.getValue()>=0.044:
